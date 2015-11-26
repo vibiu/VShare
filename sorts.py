0f7e8cb6c44428dfa228e78509d8a50ad5957584
vibiu <540540312@qq.com>
2015-11-26 23:45:45 +0800

    3.0

--- sorts.py
+++ sorts.py
@@ -7,7 +7,7 @@ def randomli(num):
 	for i in range(0, num):
 		lis.append(random.randint(1, num))
 	return lis
-	
+
 def bubbleSortUp(li):
 	n = len(li)
 	for i in range(n):
@@ -23,14 +23,13 @@ def bubbleSortDown(li):
 				li[j - 1], li[j] = li[j], li[j - 1]
 	return li
 
-def selectSortDown(ary):
-	n = len(ary)
+def selectSortDown(li):
+	n = len(li)
 	for i in range(n):
 		for j in range(i + 1, n):
-			if ary[j] > ary[i]:
-				ary[i], ary[j] = ary[j], ary[i]
-	return ary
-
+			if li[j] > li[i]:
+				li[i], li[j] = li[j], li[i]
+	return li
 def selectSortUp(li):
 	n = len(li)
 	for i in range(n):
@@ -41,15 +40,45 @@ def selectSortUp(li):
 		li[max], li[i] = li[i], li[max]
 	return li
 
+def insertSortUp(ary):
+	for i in range(1, len(ary)):
+		if ary[i] < ary[i - 1]:
+			temp = ary[i]
+			index = i
+			for j in range(i - 1, - 1, -1):
+				if ary[j] >temp:
+					ary[j + 1] = ary[j]
+					index = j
+				else: break
+			ary[index] = temp
+	return ary
+def insertSortDown(ary):
+	for i in range(1, len(ary)):
+		if ary[i] > ary[i - 1]:
+			temp = ary[i]
+			index = i
+			for j in range(i-1, -1, -1):
+				if ary[j] < temp:
+					ary[j + 1] = ary[j]
+					index = j
+				else: break
+			ary[index] = temp
+	return ary
 
+t = randomli(10)
+print t, "\n"
 
-t = randomli(5)
-print t
 t = bubbleSortUp(t)
 print t
 t = bubbleSortDown(t)
-print t
+print t, "\n"
+
 t = selectSortUp(t)
 print t
 t = selectSortDown(t)
+print t, "\n"
+
+t = insertSortUp(t)
 print t
+t = insertSortDown(t)
+print t, "\n"
