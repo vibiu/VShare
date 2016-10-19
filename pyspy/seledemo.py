e7a5d5ac79da389b2f8c2868f0325b589444a2cc
vibiu <vibiu_ubuntu@163.com>
2016-10-20 04:44:03 +0800

    reset

--- pyspy/seledemo.py
+++ pyspy/seledemo.py
@@ -1,6 +1,4 @@
 from selenium import webdriver
-
-#Following are optional required
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
@@ -12,14 +10,15 @@ password = 'password'
 xpaths = {
     'username_box': '//input[@name="username"]',
     'password_box': '//input[@name="password"]',
-    'submitButton' : '//button[@id="loginBtn"]'
+    'submitButton': '//button[@id="loginBtn"]'
 }
 
 mydriver = webdriver.Firefox()
 mydriver.get(baseurl)
 mydriver.maximize_window()
 
-mydriver.find_element_by_xpath(xpaths['username_box']).clear()
+mydriver.find_element_by_xpath(
+    xpaths['username_box']).clear()
 mydriver.find_element_by_xpath(xpaths['username_box']).send_keys(username)
 
 
